64d186d717b70d230efd3b5072790898bcf7a475
Edward Hanson <hans18edward@gmail.com>
2024-05-31 12:35:08 +0000

    Added taks.js to render and style various taks items Imported TaskItems to App.js

--- MyApp/App.js
+++ MyApp/App.js
@@ -1,11 +1,36 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ScrollView,
+  TextInput,
+  Button,
+} from "react-native";
+import TaskItem from "./components/Task";
 
 export default function App() {
+  const tasks = [
+    { id: "1", title: "Go for a run" },
+    { id: "2", title: "Complete assignment" },
+    { id: "3", title: "Work on project" },
+    { id: "4", title: "Cook dinner" },
+    { id: "5", title: "Read a book" },
+  ];
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text style={styles.header1}>Hello, Devs</Text>
+      <Text style={styles.tasks}>14 tasks today</Text>
       <StatusBar style="auto" />
+      <TextInput style={styles.input} placeholder="Search tasks..." />
+      <ScrollView style={styles.container}>
+        <Text style={styles.header2}>Ongoing Task</Text>
+        {tasks.map((task) => (
+          <TaskItem key={task.id} title={task.title} />
+        ))}
+        <Button title="Add Task" onPress={() => {}} />
+      </ScrollView>
     </View>
   );
 }
@@ -13,8 +38,37 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#696969",
+    paddingTop: 50,
+  },
+  header1: {
+    fontSize: 24,
+    fontWeight: "bold",
+    textAlign: "left",
+    marginVertical: 20,
+    marginTop: -10,
+    marginHorizontal: 10,
+  },
+  header2: {
+    fontSize: 24,
+    fontWeight: "bold",
+    marginLeft: 10,
+    marginVertical: 20,
+  },
+  input: {
+    height: 40,
+    borderColor: "#fff",
+    borderWidth: 1,
+    borderRadius: 10,
+    marginHorizontal: 20,
+    marginBottom: 20,
+    backgroundColor: "#f9f9f9",
+    paddingHorizontal: 10,
+    width: 240,
+  },
+  tasks: {
+    marginTop: -17,
+    marginBottom: 20,
+    marginHorizontal: 10,
   },
 });
---
+++ MyApp/components/Task.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { StyleSheet, View, Text } from "react-native";
+
+const TaskItem = ({ title }) => {
+  return (
+    <View style={styles.taskContainer}>
+      <Text style={styles.taskText}>{title}</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  taskContainer: {
+    backgroundColor: "#f9f9f9",
+    padding: 20,
+    marginVertical: 10,
+    marginHorizontal: 20,
+    borderRadius: 10,
+    borderColor: "#ddd",
+    height: 100,
+    borderWidth: 1,
+    shadowColor: "#000",
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.1,
+    shadowRadius: 4,
+    elevation: 3,
+  },
+  taskText: {
+    fontSize: 16,
+    color: "#333",
+    fontWeight: "bold",
+    marginVertical: 15,
+  },
+});
+
+export default TaskItem;
